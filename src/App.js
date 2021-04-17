b1138618f26dec13e611c97ff3f0718bc2e3cd95
Anton-Vovk <mr.vovk.anton@gmail.com>
2021-04-17 17:05:44 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam,
+          obcaecati dolorum assumenda vitae aspernatur, aliquid numquam
+          explicabo, facere tenetur unde dolorem quo! Sit iusto natus at,
+          aliquam, repellendus repellat ipsa eligendi dolorem tempore atque
+          reprehenderit nulla magnam reiciendis, aliquid minus tenetur ipsam
+          fuga. Quas vel, sunt voluptatum debitis incidunt numquam?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
   );
-}
+};
 
 export default App;
